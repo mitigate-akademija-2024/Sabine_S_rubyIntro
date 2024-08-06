c2e3b73cefc34a8950a385e3f7b8fb00cb9bb523
xsabx <113295583+xsabx@users.noreply.github.com>
2024-08-06 07:24:42 +0000

    commit2

--- Person.rb
+++ Person.rb
@@ -1,28 +1,17 @@
-# person.rb
 class Person
-    def initialize(first_name, last_name)
-      @first_name = first_name
-      @last_name = last_name
-    end
+  attr_accessor :first_name, :last_name
   
-    def first_name
-      @first_name
-    end
-  
-    def last_name
-      @last_name
-    end
-  
-    def first_name=(value)
-      @first_name = value
-    end
-  
-    def last_name=(value)
-      @last_name = value
-    end
-  
-    def say_hello
-      puts "Hello, my name is #{@first_name} #{@last_name}."
-    end
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def say_hello
+    puts "Hello, my name is #{@first_name} #{@last_name}."
   end
   
+end
+
+person = Person.new("John", "Doe")
+person.say_hello 
+
